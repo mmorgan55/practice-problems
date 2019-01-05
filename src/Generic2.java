feaf4231699ce6fefe3d5944fcfff7d7a7761fd8
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-04 20:38:00 -0700

    Added solution from CodingBat

--- src/Generic2.java
+++ src/Generic2.java
@@ -6,4 +6,26 @@ public class Generic2 {
     return Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
   }
 
+  public int maxSpan(int[] nums) {
+    if (nums.length == 0) {
+      return 0;
+    }
+
+    int maxSpan = 1;
+
+    for (int i = 0; i < nums.length; i++) {
+      int span = 0;
+      for (int j = i; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+          if ((j - i) >= maxSpan) {
+            maxSpan = (j - i) + 1;
+          }
+        }
+      }
+    }
+
+    return maxSpan;
+  }
+
+
 }
