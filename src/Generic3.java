0bce99ce28505559d35da255a98ee242f501d5a1
Matthew Morgan <mmorgan55@cnm.edu>
2019-01-05 23:04:33 -0700

    Added HackerRank solution

--- src/Generic3.java
+++ src/Generic3.java
@@ -1,3 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Generic3 {
+
+  int sockMerchant(int n, int[] ar) {
+    Set<Integer> set = new HashSet<>();
+    int numPairs = 0;
+
+    for(int i : ar) {
+      if(set.contains(i)) {
+        set.remove(i);
+        numPairs++;
+      } else {
+        set.add(i);
+      }
+    }
+
+    return numPairs;
+  }
 
 }
